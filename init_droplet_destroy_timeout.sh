728e1861062ede8065b4a1b6929f4c140913ae06
icqparty <icqparty@gmail.com>
2019-05-01 10:20:09 +0300

    fix script

--- init_droplet_destroy_timeout.sh
+++ init_droplet_destroy_timeout.sh
@@ -6,15 +6,14 @@ NAME_TAG_DROPLET=${NAME_TAG_DROPLET:-""}
 TIMEOUT_DROPLET_DESTROY=${TIMEOUT_DROPLET_DESTROY:-"3600"}
 
 
-printf "Delay destroy ${TIMEOUT_DROPLET_DESTROY}\n'
+printf "Delay destroy ${TIMEOUT_DROPLET_DESTROY}\n"
 
 sleep ${TIMEOUT_DROPLET_DESTROY}
 
 printf "init droplet destroy\n"
 
-time curl -X DELETE -H "Content-Type: application/json" \
+curl -X DELETE -H "Content-Type: application/json" \
                      -H "Authorization: Bearer ${TOKEN_DIGITAL_OCEAN}" \
                      "https://api.digitalocean.com/v2/droplets?tag_name=${NAME_TAG_DROPLET}"
 
 printf "\nno destroed\n"
-
